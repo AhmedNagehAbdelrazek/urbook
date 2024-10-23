626041d80d868b006919d2eabc7c1a755bfda86b
mohamed ahmed <m.ahmed20201863@gmail.com>
2024-10-23 20:32:44 +0300

    Add localization support for multiple languages

--- mobile/urbook/lib/core/utils/assets_manager/assets_constant.dart
+++ mobile/urbook/lib/core/utils/assets_manager/assets_constant.dart
@@ -1,3 +1,4 @@
 class AssetsConstant {
   static const String fontName = 'PlusJakartaSans';
+  static const String translationPath = 'assets/translation';
 }
--- mobile/urbook/lib/main.dart
+++ mobile/urbook/lib/main.dart
@@ -1,23 +1,40 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:urbook/core/routes/app_router.dart';
 import 'package:urbook/core/routes/page_route_name.dart';
+import 'package:urbook/core/themes/application_theme_manager.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'core/utils/assets_manager/assets_constant.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(EasyLocalization(
+      supportedLocales: const [
+        Locale('en'),
+        Locale('ar'),
+        Locale('fr'),
+        Locale('ru'),
+      ],
+      fallbackLocale: const Locale('en'),
+      path: AssetsConstant.translationPath,
+      child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       debugShowCheckedModeBanner: false,
       title: 'ur book',
       initialRoute: PageRouteName.initial,
       onGenerateRoute: AppRouter.onGenerateRoute,
+      theme: ApplicationThemeManager.lightThemeData,
     );
   }
 }
-
