074f8d10efd15e6667d8754a36b0c8367fa9336d
mohamed ahmed <m.ahmed20201863@gmail.com>
2024-10-23 20:03:15 +0300

    add images and images_paths.dart

--- mobile/urbook/lib/core/utils/assets_manager/image_paths.dart
+++ mobile/urbook/lib/core/utils/assets_manager/image_paths.dart
@@ -1,3 +1,10 @@
 class ImagePaths {
   static const String logo = 'assets/images/quickmart.png';
+  static const String emptyWishlist = 'assets/images/empty_wishlist.png';
+  static const String lock = 'assets/images/lock.png';
+  static const String onboarding1 = 'assets/images/Onboarding1.png';
+  static const String onboarding2 = 'assets/images/Onboarding2.png';
+  static const String onboarding3 = 'assets/images/Onboarding3.png';
+  static const String successfullyOrder = 'assets/images/successfully_order.png';
+  static const String emptyCart = 'assets/images/empty_cart.png';
 }
